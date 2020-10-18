a21c0e05ac7925cc082e8d2ca576fbad5416fe8b
ZHB <893821296@qq.com>
2020-10-19 00:14:20 +0800

    页面跳转时加载动画效果

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd, NavigationStart  } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent {
   title = 'ZKCX';
+  showLoading = false;
   constructor(private router: Router) {
 
   }
+  
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
@@ -17,7 +20,19 @@ export class AppComponent {
     // let token = sessionStorage.getItem('token')
     // if (token == null) {
     //   this.router.navigate(["/"]);
+    
     // }
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        // 每次路由跳转开始改变状态
+        this.showLoading = true;
+      
+      }
+      if (event instanceof NavigationEnd) {
+        // 每次路由跳转结束改变状态
+        this.showLoading = false;
+      }
+    });
   }
   ngOnDestroy() { 
     sessionStorage.setItem('aaa', '离开项目')
